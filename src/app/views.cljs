dca72db059fc6c895b05a1fd82c4bfb25129be33
Brunya <zbruno.linux@gmail.com>
2020-06-23 17:08:35 +0200

    react component in reagent

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,6 +1,11 @@
 (ns app.views
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            ["react-grid-layout" :as rgl :default GridLayout]
+            [packery :as Packery]))
 
 (defn app []
-  [:div {:style {:display "flex" :justify-content "center"}}
-   [:h1 "Welcome"]])
+  (js/console.log GridLayout)
+  [:> GridLayout {:cols 12 :rowHeight 30 :width 1200}
+   [:div (:data-grid {:x 0 :y 0 :w 1 :h 2 :static true}) "a"]
+   [:div (:data-grid {:x 1 :y 0 :w 3 :h 2 :minW 2 :maxW 4}) "b"]
+   [:div (:data-grid {:x 4 :y 0 :w 1 :h 2}) "c"]])
