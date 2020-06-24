a3faf1ff655629dcdc2f5087d9f73294d9b47cc5
Brunya <zbruno.linux@gmail.com>
2020-06-24 11:55:27 +0200

    mvp

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,11 +1,9 @@
 (ns app.views
   (:require [reagent.core :as reagent :refer [atom]]
-            ["react-grid-layout" :as rgl :default GridLayout]
-            [packery :as Packery]))
+            ["react-grid-layout" :as GridLayout]))
 
 (defn app []
-  (js/console.log GridLayout)
-  [:> GridLayout {:cols 12 :rowHeight 30 :width 1200}
-   [:div (:data-grid {:x 0 :y 0 :w 1 :h 2 :static true}) "a"]
-   [:div (:data-grid {:x 1 :y 0 :w 3 :h 2 :minW 2 :maxW 4}) "b"]
-   [:div (:data-grid {:x 4 :y 0 :w 1 :h 2}) "c"]])
+  [:> GridLayout {:cols 5 :rowHeight 30 :width 1200}
+   ^{:key "a"} [:div.a (:data-grid {:x 0 :y 0 :w 1 :h 2 :static true}) "a"]
+   ^{:key "b"} [:div.b (:data-grid {:x 1 :y 0 :w 3 :h 2 :minW 2 :maxW 4}) "b"]
+   ^{:key "c"} [:div.c (:data-grid {:x 4 :y 0 :w 1 :h 2}) "c"]])
