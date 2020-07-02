b1376dfeaa76898e7b4f9452a53718aafdf85db1
marcelllaks <marcelllakos@gmail.com>
2020-07-02 13:40:13 +0200

    major changes

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -2,12 +2,15 @@
   (:require [reagent.core :as reagent :refer [atom]]
             ["react-grid-layout" :as GridLayout]
             [clojure.string :refer [join]]
-            ["chart.js" :as Chart]))
+            ["chart.js" :as Chart]
+            [brave.cmc :as cmc]))
 
 ;BOGUS DATA--------------------------------------------------------------------------------------------
+(def app-state (cmc/init {:apikey "testing-the-board" :host "cc.zgen.hu:7000" :protocol :https}))
 (def data (atom {:feri {:browserName "Rókás böngésző" :browserLang "Lovari" :device "Mobil" :os "Linux" :time 1593460888909 :cookie? "igen" :siteLocation "zgen.hu"} :valaki1 {:browserName "Chromium" :time 1591459607082 :device "Táblagép gec" :os "Almás oprendszer" :cookie? "nem" :siteLocation "zgen.hu"} :bela {:browserName "Opera" :time 1593359607082 :browserLang "Meginlovari" :device "Pc" :os "Ablakok" :cookie? "igen" :siteLocation "Zegen.com"}}))
 ;(def datasum (atom {:browserName (list "Chromium"  "Chrome" "Edge" "Explorer" "Explorer") :valami (list "asd" "aasd" "aaasd")}))
-(def colorvector (atom ["#00ADB5" "#E8F8F5" "#D1F2EB" "#A3E4D7" "#76D7C4" "#48C9B0" "#1ABC9C" "#17A589" "#148F77" "#117864" "#0E6251" "#117864"]))
+(def colorvector (atom ["#00ADB5" "#E8F8F5" "#A3E4D7" "#D1F2EB" "#76D7C4" "#48C9B0" "#1ABC9C" "#17A589" "#148F77" "#117864" "#0E6251" "#117864"]))
+(def days (atom ["Error" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"]))
 ;:siteLocation '() :osName '() :cpuCores '() :browserHeight '() :browserWidth '() :deviceManufacturer '() :screenHeight '() :screenWidth '() :cookies? '() :cookies '() :colorDepth '() :pixelDepth '() :pathName '() :clientTime '() :referrer '() :prevSites '() :protocol '() :browserLang '()
 
 ;FUNCTIONS---------------------------------------------------------------------------------------------
@@ -46,21 +49,16 @@
   (let [list (atom ())]
     (doseq [i (keys @data)]
       (reset! list (conj @list (get-in @data [i :time]))))
-    (let [val (atom {:1 0 :2 0 :3 0})]
+    (let [val (atom {:1 0 :2 0 :3 0 :4 0 :5 0})]
       (doseq [i @list]
-        (when (> 1 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:1] inc))
-        (when (> 2 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:2] inc))
-        (when (> 3 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:3] inc))
-        (when (> 4 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:4] inc))
-        (when (> 5 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:5] inc)))
-      (into [] (vals @val)))))
+        (if (> 1 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:1] inc) (if (> 2 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:2] inc) (if (> 3 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:3] inc) (if (> 4 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:4] inc) (if (> 5 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:5] inc)))))))
+      (into [] (reverse (vals @val))))))
 
-(defn days []
-  (let [val (atom ())]
-    (into [] (join (list (.getMonth (js/Date.)) (.getDate (js/Date.)))))))
+(defn set-item! [key val]
+  (.setItem (.-localStorage js/window) key val))
 
-
-
+(defn get-item [key]
+  (.getItem (.-localStorage js/window) key))
 
 ;DEVICES CHART-----------------------------------------------------------------------------------------
 (defn show-revenue-chart-devices
@@ -70,7 +68,9 @@
                     :data {
                            :labels (labelvector "device")
                            :datasets [{:data (datavector "device")
-                                       :backgroundColor @colorvector}]}}]
+                                       :backgroundColor @colorvector}]}
+                    :options {:legend {:display true :position "top" :align "center" :labels {:fontColor "white"}}}}]
+
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -91,7 +91,8 @@
                     :data {
                            :labels (labelvector "browserName")
                            :datasets [{:data (datavector "browserName")
-                                       :backgroundColor @colorvector}]}}]
+                                       :backgroundColor @colorvector}]}
+                    :options {:legend {:display true :position "top" :align "center" :labels {:fontColor "white"}}}}]
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -112,7 +113,9 @@
                     :data {
                            :labels (labelvector "os")
                            :datasets [{:data (datavector "os")
-                                       :backgroundColor @colorvector}]}}]
+                                       :backgroundColor @colorvector}]}
+                    :options {:legend {:display true :position "top" :align "center" :labels {:fontColor "white"}}}}]
+
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -152,8 +155,17 @@
   (let [context (.getContext (.getElementById js/document "rev-chartjs-line") "2d")
         chart-data {:type "line"
                     :data {
-                           :labels [(join (list (.getMonth (js/Date.)) "." (.getDate (js/Date.))))]
-                           :datasets [{:data (time-to)}]}}]
+                           :labels [(nth @days (if (> 1 (- (.getDay (js/Date.)) 4)) (+ 7 (- (.getDay (js/Date.)) 4)) (- (.getDay (js/Date.)) 4)))
+                                    (nth @days (if (> 1 (- (.getDay (js/Date.)) 3)) (+ 7 (- (.getDay (js/Date.)) 3)) (- (.getDay (js/Date.)) 3)))
+                                    (nth @days (if (> 1 (- (.getDay (js/Date.)) 2)) (+ 7 (- (.getDay (js/Date.)) 2)) (- (.getDay (js/Date.)) 2)))
+                                    (nth @days (if (= 1 (.getDay (js/Date.))) 7 (- (.getDay (js/Date.)) 1)))
+                                    (nth @days (.getDay (js/Date.)))]
+
+                           :datasets [{
+                                       :backgroundColor "#00ADB5"
+                                       :minBarlength "50"
+                                       :data (time-to)}]}
+                    :options {:legend {:display false} }}]
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -163,7 +175,7 @@
     {:component-did-mount #(show-revenue-chart-line)
      :display-name        "chartjs-component-line"
      :reagent-render      (fn []
-                            [:canvas {:id "rev-chartjs-line" :width "100%" :height "33%"}])}))
+                            [:canvas {:id "rev-chartjs-line" :width "auto" :height "100%"}])}))
 
 
 ;COOKIE CHART-----------------------------------------------------------------------------------------
@@ -176,7 +188,9 @@
                            :labels (labelvector "cookie?")
                            :datasets [{
                                        :data (datavector "cookie?")
-                                       :backgroundColor @colorvector}]}}]
+                                       :backgroundColor @colorvector}]}
+                    :options {:legend {:display false} :scaleOptions {:yAxes [{:ticks {:beginAtZero true}}]}}}]
+
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -192,7 +206,7 @@
 
 (defn app []
 
-  [:> GridLayout {:cols 5 :rowHeight 200 :width (-> js/screen .-availWidth)}
+  [:> GridLayout {:cols 5 :rowHeight 210 :width (-> js/screen .-availWidth)}
 
    ^{:key "a"} [:div.kartya {:data-grid {:x 0 :y 0 :w 1 :h 2}}
                 [:div.pageName "All views"][:div.bigNumber {:class [(when (< 2 (count (str (count @data)))) "longnumber")]} (count @data)][:div.active "Active"][:div.activeCount (timecounter "active")]]
@@ -201,7 +215,7 @@
                                                               [:div.newOld [:div.allSites "Old User"] [:div.bigNumber2 "69"] [:div.allViews "In the last 24 hour"]]]
 
    ^{:key "x"} [:div.kartya {:data-grid {:x 1 :y 0 :w 1 :h 2}}[:div.browser [:div.browserName "Browsers"][:div.browserGraph [(rev-chartjs-component-browser)]]]]
-   ^{:key "h"} [:div.kartya {:data-grid {:x 2 :y 0 :w 1 :h 2}}[:div.devices [:div.devicesName "Devices"][:div.devicesGraph [#(rev-chartjs-component-devices)]]]]
+   ^{:key "h"} [:div.kartya {:data-grid {:x 2 :y 0 :w 1 :h 2}}[:div.devices [:div.devicesName "Devices"][:div.devicesGraph [(rev-chartjs-component-devices)]]]]
 
    ^{:key "i"} [:div.kartya {:data-grid {:x 3 :y 0 :w 1 :h 2}}[:div.os [:div.osName "Operating System"][:div.osGraph [#(rev-chartjs-component-os)]]]]
 
@@ -218,7 +232,9 @@
 
 
 (defn app1 []
-  (-> js/screen .-height))
+
+  (set-item! "kulcs" "ertek")
+  (get-item "kulcs"))
 
 
 
