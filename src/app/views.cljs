99de69ccb4460bfe09bfb8ff7696b9bacccead27
marcelllaks <marcelllakos@gmail.com>
2020-07-19 22:02:45 +0200

    refactored

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,20 +1,31 @@
 (ns app.views
   (:require [reagent.core :as reagent :refer [atom]]
             ["react-grid-layout" :as GridLayout]
-            [clojure.string :refer [join]]
+            [clojure.string :refer [join split]]
             ["chart.js" :as Chart]
-            [brave.cmc :as cmc]
-            [clojure.string :as str]))
+            [brave.cmc :as cmc]))
 
-;BOGUS DATA--------------------------------------------------------------------------------------------
+
+;DATA------------------------------------------------------------------------------------------------
 
 (def data (cmc/init {:apikey "testing-the-board" :host "cc.zgen.hu" :protocol :https :reagent? true}))
 (def state (atom {:darkmode true}))
 (def colorvector (atom ["#00ADB5" "#E8F8F5" "#7ee8ed" "#D1F2EB" "#76D7C4" "#48C9B0" "#1ABC9C" "#17A589" "#148F77" "#117864" "#0E6251" "#117864"]))
-(def daylist (atom ["Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat"]))
 
 ;----------------------------------------------BOGUS DATA FUNCTIONS----------------------------------
 
+(defn browser-checker []
+ (let [userAgent (str (-> js/navigator .-userAgent))]
+  (cond
+    (clojure.string/includes? userAgent "SeaMonkey") "SeaMonkey"
+    (clojure.string/includes? userAgent "Chromium") "Chromium"
+    (clojure.string/includes? userAgent "Firefox") "Firefox"
+    (clojure.string/includes? userAgent "Chrome") "Chrome"
+    (clojure.string/includes? userAgent "Safari") "Safari"
+    (or (clojure.string/includes? userAgent "OPR") (clojure.string/includes? userAgent "Opera")) "Opera"
+    (or (clojure.string/includes? userAgent "MSIE") (clojure.string/includes? userAgent "Trident")) "Internet Explorer"
+    (clojure.string/includes? userAgent "Edg") "Microsoft Edge"
+    :else "Unknown browser")))
 
 (defn adatbazisadd []
   (let [newuser (atom {:key false})]
@@ -22,11 +33,11 @@
       (when (nil? (.getItem (.-localStorage js/window) "zgen")) ((reset! newuser {:key true})(.setItem (.-localStorage js/window) "zgen" (.getTime (js/Date.))))))
    (swap! data assoc (if (-> js/navigator .-cookieEnabled) (keyword (.getItem (.-localStorage js/window) "zgen")) (keyword (str (.getTime (js/Date.)))))
     {:newuser (:key @newuser)
-     :browserName "SeaMonkey"
+     :browserName (browser-checker)
      :siteLocation (-> js/window .-location .-hostname)
      :osName (-> js/navigator .-platform)
      :cpuCores (-> js/navigator .-hardwareConcurrency)
-     :deviceManufacturer (case (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
+     :deviceManufacturer (cond (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
      :screenHeight (-> js/screen .-height)
      :screenWidth (-> js/screen .-width)
      :cookies? (-> js/navigator .-cookieEnabled)
@@ -43,13 +54,12 @@
 
 (defn extraadd []
   (let [newuser (atom {:key true})]
-    (swap! data assoc (keyword (str (.getTime (js/Date.)))) {:newuser (:key @newuser)
+    (swap! data assoc (keyword (str (.getTime (js/Date.)))) {:newuser true
                                                              :browserName (nth (list "SeaMonkey" "Chrome" "Chromium" "Firefox" "Microsoft Edge" "Opera" "Safari" "Internet Explorer") (rand-int 8))
                                                              :siteLocation (nth (list "zgen.hu" "incognito" "zawiasa.hu") (rand-int 3))
                                                              :osName (nth (list "Windows" "MacOS" "Android") (rand-int 3))
                                                              :cpuCores (-> js/navigator .-hardwareConcurrency)
                                                              :deviceManufacturer (nth (list "mobile" "desktop" "tablet") (rand-int 3))
-                                                             ; (case (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
                                                              :screenHeight (-> js/screen .-height)
                                                              :screenWidth (-> js/screen .-width)
                                                              :cookies? false
@@ -64,12 +74,13 @@
                                                              :browserLang (-> js/navigator .-language)
                                                              :time (- (.getTime (js/Date.)) (rand-int 604800000))})))
 
-
 (defn adatbazisdel []
   (reset! data {}))
 
-;------------------------------------------_END--------------------------------------------
+(defn remove-item! []
+  (.removeItem (.-localStorage js/window) "zgen"))
 
+;------------------------------------------_END--------------------------------------------
 
 ;------------------------------------------------------------------------------------------------------
 ;---------------------------------------------FUNCTIONS------------------------------------------------
@@ -97,7 +108,6 @@
         (when (>= (cond (= mode "day") 86400000
                         (= mode "week") 604800000
                         (= mode "active") 600000
-;                        (= mode "active") 5000
                         (= mode "month") 2629746000) (- (.getTime (js/Date.)) (nth @list i)) (reset! val (conj @val (nth @list i))))))
       (str (count @val)))))
 
@@ -107,7 +117,6 @@
       (reset! list (conj @list (get-in @data [i (keyword key)]))))
     (let [val (atom {:true 0 :false 0})]
       (doseq [i (range (count @list))]
-        (print (keyword (str (nth @list i))))
         (if (nth @list i) (swap! val update :true inc) (swap! val update :false inc)))
       (into [] (vals @val)))))
 
@@ -120,17 +129,19 @@
         (if (> 1 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:1] inc) (if (> 2 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:2] inc) (if (> 3 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:3] inc) (if (> 4 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:4] inc) (if (> 5 (/ (- (.getTime (js/Date.)) i) 86400000)) (swap! val update-in [:5] inc)))))))
       (into [] (reverse (vals @val))))))
 
+(defn dynamicText [size text]
+  (join (list (- size (* 30 (count (str text)))) "px")))
+
 (defonce timer (atom (js/Date.)))
 
 (defonce time-updater (js/setInterval
                        #(reset! timer (js/Date.)) 1000))
 
 (defn clock []
-  (let [time-str (-> @timer .toTimeString (str/split " ") first)]
-    [:div.example-clock
+  (let [time-str (-> @timer .toTimeString (split " ") first)]
+    [:div
      {:style {:color "#00ADB5"}}
      time-str]))
-
 
 ;------------------------------------------------------------------------------------------------------
 ;---------------------------------------------CHARTS---------------------------------------------------
@@ -147,10 +158,8 @@
                            :datasets [{:data (tovector "val" "deviceManufacturer")
                                        :backgroundColor @colorvector}]}
                     :options {:animation {:duration 0}
-                              :legend {:display true :position "top" :align "center" :labels {:fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
-
+                              :legend {:display true :position "top" :align "center" :labels {:fontSize 20 :fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
       (js/Chart. context (clj->js chart-data))))
-
 
 (defn rev-chartjs-component-devices
   []
@@ -167,13 +176,12 @@
   (let [context (.getContext (.getElementById js/document "rev-chartjs-browser") "2d")
         chart-data {:type "doughnut"
                     :data {
-                           :labels (tovector "key" "browserName")
+                           :labels (take 3 (tovector "key" "browserName"))
                            :datasets [{:data (tovector "val" "browserName")
                                        :backgroundColor @colorvector}]}
                     :options {:animation {:duration 0}
-                              :legend {:display true :position "top" :align "center" :labels {:fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
+                              :legend {:display true :position "top" :align "center" :labels {:fontSize 20 :fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
       (js/Chart. context (clj->js chart-data))))
-
 
 (defn rev-chartjs-component-browser
   []
@@ -194,10 +202,9 @@
                            :datasets [{:data (tovector "val" "osName")
                                        :backgroundColor @colorvector}]}
                     :options {:animation {:duration 0}
-                              :legend {:display true :position "top" :align "center" :labels {:fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
+                              :legend {:display true :position "top" :align "center" :labels {:fontSize 20 :fontColor (if (not (:darkmode @state)) "#738598" "white")}}}}]
 
       (js/Chart. context (clj->js chart-data))))
-
 
 (defn rev-chartjs-component-os
   []
@@ -208,6 +215,9 @@
                             [:canvas {:id "rev-chartjs-os" :width "100%" :height "100%"}])}))
 
 ;ALL CHART -----------------------------------------------------------------
+(defn getDay [day]
+    (nth '("Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat")
+      (if (> 0 (- (.getDay (js/Date.)) day)) (+ 7 (- (.getDay (js/Date.)) day)) (- (.getDay (js/Date.)) day))))
 
 (defn show-revenue-chart-line
   []
@@ -215,11 +225,11 @@
         chart-data {:type "line"
                     :data {
 
-                           :labels [(nth @daylist (if (> 0 (- (.getDay (js/Date.)) 4)) (+ 7 (- (.getDay (js/Date.)) 4)) (- (.getDay (js/Date.)) 4)))
-                                    (nth @daylist (if (> 0 (- (.getDay (js/Date.)) 3)) (+ 7 (- (.getDay (js/Date.)) 3)) (- (.getDay (js/Date.)) 3)))
-                                    (nth @daylist (if (> 0 (- (.getDay (js/Date.)) 2)) (+ 7 (- (.getDay (js/Date.)) 2)) (- (.getDay (js/Date.)) 2)))
-                                    (nth @daylist (if (= 0 (.getDay (js/Date.))) 6 (- (.getDay (js/Date.)) 1)))
-                                    (nth @daylist (.getDay (js/Date.)))]
+                           :labels [(getDay 4)
+                                    (getDay 3)
+                                    (getDay 2)
+                                    (getDay 1)
+                                    (getDay 0)]
 
                            :datasets [{
                                        :backgroundColor "#00ADB5"
@@ -233,7 +243,6 @@
                                                         :beginAtZero true}}]}}}]
       (js/Chart. context (clj->js chart-data))))
 
-
 (defn rev-chartjs-component-line
   []
   (reagent/create-class
@@ -241,7 +250,6 @@
      :display-name        "chartjs-component-line"
      :reagent-render      (fn []
                             [:canvas {:id "rev-chartjs-line" :width "auto" :height "90%"}])}))
-
 
 ;COOKIE CHART-----------------------------------------------------------------------------------------
 
@@ -254,7 +262,6 @@
                            :datasets [{
                                        :data (truecounter "cookies?")
                                        :backgroundColor @colorvector}]}
-;                    :options {:legend {:display false} :scaleOptions {:yAxes [{:ticks {:beginAtZero true :autoSkip false}}]}}
 
                     :options {:animation {:duration 0}
                               :legend {:display false}
@@ -262,10 +269,6 @@
                                        :yAxes [{
                                                 :ticks {
                                                         :beginAtZero true}}]}}}]
-
-
-
-
       (js/Chart. context (clj->js chart-data))))
 
 
@@ -296,27 +299,27 @@
      :reagent-render      (fn []
                             [:canvas {:id "rev-chartjs-crypto" :width "auto" :height "90%"}])}))
 
-
-
 ;--------------------------------------------------------------------------------------------------
 
 (defn app []
    [:div {:class [(if (:darkmode @state) "dark" "light")]}
-    [:button {:on-click #(adatbazisdel) } "Torol"]
-    [:button {:on-click #(adatbazisadd)} "Hozzaad"]
+    [:button {:on-click #(adatbazisdel) } "Database del"]
+    [:button {:on-click #(adatbazisadd)} "Local-Storage add"]
+    [:button {:on-click #(remove-item!)} "Local-Storage del"]
     [:button {:on-click #(extraadd)} "Extra data"]
+    [:button {:on-click #(doseq [i (range 10)] (extraadd))} "10x extra data"]
+    [:button {:on-click #(doseq [i (range 100)] (extraadd))} "100x extra data"]
     [:> GridLayout {:cols (if (>= (-> js/screen .-availWidth) 3840) 10 5) :className "grid" :rowHeight 210 :width (if (= 0 (+ (-> js/window .-screenY) (-> js/window .-screenTop))) (-> js/screen .-width) (-> js/screen .-availWidth))}
-
 
   ;One Page Card
       ^{:key "a"}
       [:div.card.column {:data-grid {:x 0 :y 0 :w 1 :h 2}}
        [:div.page
         [:h1.cardTitle "All views"]
-        [:h1.cardNumber {:class [(when (< 2 (count (str (count @data)))) "longnumber")]} (count @data)]
+        [:h1.cardNumber {:style {:font-size (dynamicText 250 (count @data))}} (count @data)]
         [:p.cardText "Active"]
         (let [active (atom (str (userselector 0 "active")))]
-          (js/setInterval #(reset! active (str (userselector 0 "active"))) 1000)
+          (js/setInterval #(reset! active (str (userselector 0 "active"))) 5000)
           [:p.active (str @active)])]]
 
   ;New/Old Users Card
@@ -324,11 +327,11 @@
       [:div.card.row {:data-grid {:x 3 :y 0 :w 1 :h 1}}
        [:div.users
         [:h1.cardTitle "New User"]
-        [:h1.cardNumber {:class [(when (< 2 (count (str (userselector 1 "day")))) "longnumber")]} (userselector 1 "day")]
+        [:h1.cardNumber {:style {:font-size (dynamicText 180 (userselector 1 "day"))}} (userselector 1 "day")]
         [:p.cardText "In the last 24 hour"]]
        [:div.users
         [:h1.cardTitle "Old User"]
-        [:h1.cardNumber {:class [(when (< 2 (count (str (userselector 2 "day")))) "longnumber")]} (userselector 2 "day")]
+        [:h1.cardNumber {:style {:font-size (dynamicText 180 (userselector 2 "day"))}} (userselector 2 "day")]
         [:p.cardText "In the last 24 hour"]]]
 
   ;Timer Card
@@ -378,21 +381,21 @@
       [:div.card.row {:data-grid {:x 0 :y 2 :w 3 :h 2}}
        [:div.total
         [:h1.cardTitle "All Sites"]
-        [:h1.cardNumber (count @data)]
+        [:h1.cardNumber {:style {:font-size (dynamicText 250 (count @data))}} (count @data)]
         [:p.cardText "All Views"]]
        [:div.totalDetails
         [:div.details
          [:div.daily
           [:h1.cardTitle "All Sites"]
-          [:h1.cardNumber (userselector 0 "day")]
+          [:h1.cardNumber {:style {:font-size (dynamicText 180 (userselector 0 "day"))}} (userselector 0 "day")]
           [:p.cardText "Daily"]]
          [:div.weekly
           [:h1.cardTitle "All Sites"]
-          [:h1.cardNumber (userselector 0 "week")]
+          [:h1.cardNumber {:style {:font-size (dynamicText 180 (userselector 0 "week"))}} (userselector 0 "week")]
           [:p.cardText "Weekly"]]
          [:div.monthly
           [:h1.cardTitle "All Sites"]
-          [:h1.cardNumber (userselector 0 "month")]
+          [:h1.cardNumber {:style {:font-size (dynamicText 180 (userselector 0 "month"))}} (userselector 0 "month")]
           [:p.cardText "Monthly"]]]
         [:div.cardGraph [#(rev-chartjs-component-line)]]]]
 
@@ -410,16 +413,3 @@
            [:p.cardText "3002,25"]
            [:p.cardText "USD"]]]]
         [:div.cryptoGraph [#(rev-chartjs-component-crypto)]]]]]])
-
-
-
-(defn app1 []
-  [:p (str @data)
-    [:button {:on-click #(adatbazisdel) } "Torol"]
-    [:button {:on-click #(adatbazisadd)} "Hozzaad"]
-    [:button {:on-click #(extraadd)} "Extra data"]
-    (str (-> js/navigator .-cookieEnabled))]
-;    (str (.getDay (js/Date.)))
-;      (str (.getDate (js/Date.))))
-;    (str (js/Date.))]
-  (print (-> js/screen .-availWidth)))
